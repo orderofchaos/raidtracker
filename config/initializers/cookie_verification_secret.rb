13a4ffe5106f36d6995cf96770e9e83bfb1bf8f6
RageKage <rkage@orderofchaos.com>
2010-04-14 12:23:03 -0400

    initial commit, rails 3 skeleton

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.cookie_secret = '28cc2ce5cad4ce489d6af7e71cfb38173093c04151fc03bffa50801656ebf912252d942b4deab60489633918414f97e667cf627a59975fb15267f34cd93c9e82'
