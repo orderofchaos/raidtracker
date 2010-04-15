5e8ba0b8b2d6ac4521a6331cb8778e1e6693eefe
Nicholas McFaul <nmcfaul@rpmsystems.net>
2010-04-15 08:50:48 -0400

    fixed upgrade

--- config/initializers/cookie_verification_secret.rb
+++ config/initializers/cookie_verification_secret.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
-Rails.application.config.cookie_secret = '36277dd1c27e262c4e4b963bee8cedb75930468a72a44ccf6bfdf01cf593a163db7729fc87b431e32ed9fd187f809de38cb38b5794e6d1c4e57937f472095f3d'
+Rails.application.config.cookie_secret = 'd6a806619e59bc18caa197780ad3d118dd0672e82174fa0d93544989171a343e93774c370b31329654eb237d31d22b30db71f87ad3d96ba0faea0d83db82a5f8'
